af6b6eefa60cad9d054fe77192e223494a4fb840
Miguel mendes <ei11058@linus20.(none)>
2014-10-16 15:10:20 +0100

    Made quick working transmission method.

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -20,39 +20,20 @@
 
 volatile int STOP=FALSE;
 
-int establishConn(int fd, unsigned char* SET, unsigned char* UA) {
-	int res;
+int connAttempts = 0;
+typedef enum {CONNECTING, TRANSMITTING, CLOSING} connStage;
+connStage currentConnStage = CONNECTING;
 
-	//sending SET trama
-	puts("Sending SET trama...");
-    res = write(fd,SET,5);
-	sleep(5); 
-	if(res != -1)
-    	printf("%d bytes written\n", res);
+//frame creation
+	
 
-	alarm(3);
-
-	res = read(fd,UA,5);
-	if(res != -1)
-		printf("Established. Read %d bytes.");
-	else {
-		printf("Reading failed.");
-		return 1;
-	}
-
-	if(UA[0] == F && UA[1] == A && UA[2] == 0x07 && UA[3] == (A ^ 0x07) && UA[4] == F)
-		puts("UA trama: valid");
-	else {
-		puts("UA trama: invalid");
-		return 1;
-	}
-
-	return 0;
+int establishConn(int fd) {
+	
 }
 
-int set(int fd, unsigned char* SET, unsigned char* UA) {
+int set() {
 	puts("Trying again...");
-	establishConn(fd, &SET, &UA);
+	currentConnStage = CONNECTING;
 	return 0;
 }
 
@@ -60,9 +41,13 @@ int main(int argc, char** argv)
 {
 	puts("Beginning.");
     int fd,c;
+	unsigned char SET[5], UA[5];
+	SET[0] = F;
+	SET[1] = A;
+	SET[2] = C;
+	SET[3] = SET[1] ^ SET[2];
+	SET[4] = F;
     struct termios oldtio,newtio;
-	unsigned char* SET = malloc(5);
-	unsigned char* UA = malloc(5);
     char buf[255];
     int i, sum = 0, speed = 0;
     
@@ -104,22 +89,49 @@ int main(int argc, char** argv)
       exit(-1);
     }
 
-	puts("Creating Set trama...");
+	while(currentConnStage == CONNECTING) {
+		alarm(3);
+	
+		int res;
 
-	//trama creation
-	SET[0] = F;
-	SET[1] = A;
-	SET[2] = C;
-	SET[3] = SET[1] ^ SET[2];
-	SET[4] = F;
+		//sending SET frame
+		puts("Sending SET frame...");
+    	res = write(fd,SET,5);
+		sleep(5); 
+		if(res != -1)
+    		printf("%d bytes written\n", res);
+	
+		if(++connAttempts >= 4) {
+			puts("Deactivating alarm.");
+			alarm(0);
+			}
+	
+		puts("Reading...");
+		res = read(fd,UA,5);
+		if(res != -1)
+			printf("Established. Read %d bytes.", res);
+		else {
+			printf("Reading failed.");
+		}
+	
+		if(UA[0] == F && UA[1] == A && UA[2] == 0x07 && UA[3] == (A ^ 0x07) && UA[4] == F) {
+			puts("UA frame: valid");
+			currentConnStage == TRANSMITTING;
+		}
+		else {
+			puts("UA frame: invalid");
+			return 1;
+		}
 
-	if(establishConn(fd, &SET, &UA))
-		return 1;
+		return 0;
+		sleep(5);
+	}
     
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
     close(fd);
+
     return 0;
 }

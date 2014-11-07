36cf29177343f5b0d1f1be6699172b5f1ca3945d
Diogoei12102 <ei12102@fe.up.pt>
2014-11-07 11:02:25 +0000

    Close Correction

--- emitter/llclose.c
+++ emitter/llclose.c
@@ -1,105 +1,128 @@
 #include "llclose.h"
 
 int llclose(int fd) {
-	int res;
-	int connecting = 1;
+	//emissor
+		int res;
+		retry_count = 0;
+		connecting = 1;
 
-	//frame creation
-	unsigned char DISC[5], DISC2[5];
-	DISC[0] = F;
-	DISC[1] = A;
-	DISC[2] = C_CLOSE;
-	DISC[3] = DISC[1] ^ DISC[2];
-	DISC[4] = F;
+		//frame creation
+		unsigned char DISC[5], DISC2[5];
+		DISC[0] = F;
+		DISC[1] = A;
+		DISC[2] = C_CLOSE;
+		DISC[3] = DISC[1] ^ DISC[2];
+		DISC[4] = F;
 
-	//machine state periferal vars
-	int stop = 1, estado = 0, res2;
-	unsigned char c;
+		UA[0] = F;
+		UA[1] = A;
+		UA[2] = C_UA;
+		UA[3] = UA[1] ^ UA[2];
+		UA[4] = F;
 
-	//sending SET frame
-	puts("Sending DISC frame...");
-	res = write(fd,DISC,5);
-	if(res != -1) {
-		printf("%d bytes written\n", res);
-	} else {
-		puts("Connection establishment failed.");
-		return 1;
-	}
-			
-	//receiving UA frame
-	puts("Reading DISC frame...");
-	while(stop){
-		res = read(fd,&c,1);
-		if(res > 0) {
-			alarm(0);
-			switch(estado){
-				case 0: {
-					printf("estado 0\n");
-					if (c == F){
-						DISC2[0] = c;
-						estado = 1;
-					}
-					break;
+		puts("Setting alarm signal...");
+		signal(SIGALRM, llOpenCloseAlarmHandler); //install alarm routine
+		puts("Alarm signal set.");
+
+		//machine state periferal vars
+		int stop = 1, estado = 0, res2;
+		unsigned char c;
+
+		//sending DISC frame
+		while(retry_count <= 3) {
+			if(connecting) {
+				connecting = 0;
+
+				puts("Sending DISC frame...");
+				res = write(fd,DISC,5);
+				if(res != -1) {
+					printf("%d bytes written\n", res);
+				} else {
+					puts("Connection establishment failed.");
+					return 1;
 				}
-				case 1:{
-					printf("estado 1\n");
-					if (c == A){
-						DISC2[1] = c;
-							estado = 2;
-					} else if (c == F){
-						estado = 1;
-						DISC2[0] = c;
-					} else 
-						estado = 0;
-					break;	
-				}
-				case 2:{				
-					printf("estado 2\n");
-					if (c == C_CLOSED){
-						DISC2[2] = c;
-						estado = 3;
-					} else if (c == F){
-						DISC2[0] = c;
-						estado = 1;
-					} else
-						estado = 0; 
-					break;
-				}
-				case 3:{
-					printf("estado 3\n");
-					if (c == (DISC2[1] ^ DISC2[2])){
-						DISC2[3] = c;
-						estado = 4;
-					} else if (c == F){
-						DISC2[0] = c;
-						estado = 1;
-					} else 
-						estado = 0;
-					break;
-				}
-				case 4:{
-					printf("estado 4\n");
-					if (c == F){
-						DISC2[4] = c;
-						if (DISC2[0] == F && DISC2[1] == A && DISC2[2] == C_CLOSED && DISC2[3] == (DISC2[1] ^ DISC2[2]) && DISC2[4] == F){
-							puts("Received DISC frame successfully.");
-							stop = 0;
-							puts("Finished reading.");
+
+				alarm(3);
+				//receiving DISC frame
+				puts("Reading DISC frame...");
+				while(stop && !connecting){
+					res = read(fd,&c,1);
+					if(res > 0) {
+						alarm(0);
+						switch(estado){
+						case 0: {
+							printf("estado 0\n");
+							if (c == F){
+								DISC2[0] = c;
+								estado = 1;
+							}
 							break;
-						} else {
-							puts("DISC frame is wrong, somehow.");
-							estado = 0;
 						}
-					} else
-						estado = 0;
-					break;
+						case 1:{
+							printf("estado 1\n");
+							if (c == A){
+								DISC2[1] = c;
+								estado = 2;
+							} else if (c == F){
+								estado = 1;
+								DISC2[0] = c;
+							} else 
+								estado = 0;
+							break;	
+							   }
+						case 2:{				
+							printf("estado 2\n");
+							if (c == C_CLOSE){
+								DISC2[2] = c;
+								estado = 3;
+							} else if (c == F){
+								DISC2[0] = c;
+								estado = 1;
+							} else
+								estado = 0; 
+							break;
+							   }
+						case 3:{
+							printf("estado 3\n");
+							if (c == (DISC2[1] ^ DISC2[2])){
+								DISC2[3] = c;
+								estado = 4;
+							} else if (c == F){
+								DISC2[0] = c;
+								estado = 1;
+							} else 
+								estado = 0;
+							break;
+							   }
+						case 4:{
+							printf("estado 4\n");
+							if (c == F){
+								DISC2[4] = c;
+								if (DISC2[0] == F && DISC2[1] == A && DISC2[2] == C_CLOSE && DISC2[3] == (DISC2[1] ^ DISC2[2]) && DISC2[4] == F){
+									puts("Received DISC frame successfully.");
+									stop = 0;
+									puts("Finished reading.");
+									break;
+								} else {
+									puts("DISC frame is wrong.");
+									estado = 0;
+								}
+							} else
+								estado = 0;
+							break;
+							   }
+						}
+					}
 				}
 			}
+
+			if(estado == 4)
+				break;
 		}
-
-		if(estado == 4)
-			break;
-	}
-
-	return 0;
+			if(retry_count >= 3) {
+				puts("Connecting try count reached limit.");
+				return -1;
+			}
+			write(fd ,UA,5);
+		return 0;
 }
--- receiver/llclose.c
+++ receiver/llclose.c
@@ -2,23 +2,89 @@
 
 int llclose(int fd){
 
-	unsigned char DISC[5], c;
-	int res, res2, estado = 0, stop = 1;
+		//receptor
+		unsigned char DISC[5] ,c;
+		int res, res2, estado = 0, stop = 1;
 
-	DISC[0] = F;
-	DISC[1] = A;
-	DISC[2] = CD;
-	DISC[3] = (DISC[1] ^ DISC[2]);
-	DISC[4] = F;
+		DISC[0] = F;
+		DISC[1] = A;
+		DISC[2] = CD;
+		DISC[3] = (DISC[1] ^ DISC[2]);
+		DISC[4] = F;
 
-	puts("Receiving DISC frame...");
-	while(stop){
+		puts("Receiving DISC frame...");
+		while(stop){
+			res = read(fd,&c,1);
+			if(res != -1)
+				switch(estado){
+				case 0: {
+					printf("estado 0\n");
+					if (c == F){
+						estado = 1;
+					}
+					break;
+						}
+				case 1:{
+					printf("estado 1\n");
+					if (c == A){
+						estado = 2;
+					}
+					else if (c == F){
+						estado = 1;
+					}
+					else 
+						estado = 0;
+					break;	
+					   }
+				case 2:{				
+					printf("estado 2\n");
+					if (c == CD){
+						estado = 3;
+					}
+					else if (c == F){
+						estado = 1;
+					}
+					else
+						estado = 0; 
+					break;
+					   }
+				case 3:{
+					printf("estado 3\n");
+					if (c == (DISC[1]^DISC[2])){
+						estado = 4;
+					}
+					else if (c == F){
+						estado = 1;
+					}
+					else 
+						estado = 0;
+					break;
+					   }
+				case 4:{
+					printf("estado 4\n");
+					if (c == F){
+						puts("Sending DISC frame...");
+						res2 = write(fd,DISC,5);
+						printf("Enviou %d bytes \n",res2);
+						stop = 0;
+						break;
+					}
+					else
+						estado = 0;
+					break;
+					   }
+			}
+		}
+
+		stop = 1;
+		estado = 0;
+		while(stop){
 	    	res = read(fd,&c,1);
-		if(res != -1)
 		switch(estado){
 			case 0: {
 				printf("estado 0\n");
 				if (c == F){
+					UA[0] = c;
 					estado = 1;
 				}
 			break;
@@ -26,9 +92,11 @@ int llclose(int fd){
 			case 1:{
 				printf("estado 1\n");
 				if (c == A){
+					UA[1] = c;
 					estado = 2;
 				}
 				else if (c == F){
+					UA[0] = c;
 					estado = 1;
 				}
 				else 
@@ -37,10 +105,12 @@ int llclose(int fd){
 			}
 			case 2:{				
 				printf("estado 2\n");
-				if (c == CD){
+				if (c == C){
+					UA[2] = c;
 					estado = 3;
 				}
 				else if (c == F){
+					UA[0] = c;
 					estado = 1;
 				}
 				else
@@ -49,10 +119,12 @@ int llclose(int fd){
 			}
 			case 3:{
 				printf("estado 3\n");
-				if (c == (DISC[1]^DISC[2])){
+				if (c == (UA[1]^UA[2]){
+					UA[3] = c;
 					estado = 4;
 				}
 				else if (c == F){
+					UA[0] = c;
 					estado = 1;
 				}
 				else 
@@ -62,11 +134,14 @@ int llclose(int fd){
 			case 4:{
 				printf("estado 4\n");
 				if (c == F){
-					puts("Sending DISC frame...");
-					res2 = write(fd,DISC,5);
-					printf("Enviou %d bytes \n",res2);
-					stop = 0;
-					break;
+					UA[4] = c;
+					if (UA[0] == F && UA[1] == A && UA[2] == CS && UA[3] == (UA[1]^UA[2]) && UA[4] == F){
+						printf("UA received",res2);
+						stop = 0;
+						break;
+					}
+					else
+						estado = 0;
 				}
 				else
 					estado = 0;
@@ -75,5 +150,5 @@ int llclose(int fd){
 		}
 	}
 
-	return 0;
+		return 0;
 }

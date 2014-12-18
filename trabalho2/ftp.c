d46046f6f32ffc4225f1f2eccd1978a6587e99ab
zephiro309 <zephiro309@gmail.com>
2014-12-18 11:38:37 +0000

    Made a few warning related changes.

--- trabalho2/ftp.c
+++ trabalho2/ftp.c
@@ -1,7 +1,10 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
 	
+	char username[20], password[20], host[20], urlpath[100];
+
 	printf("Checking arguments...");
 
 	if(argc < 2 || argc > 2){
@@ -13,10 +16,12 @@ int main(int argc, char** argv) {
 
 	printf("Detecting url...");
 
-	if(sscanf(argv[1], "ftp://%[^:]:%[^@]@%[^/]/%s", user, password, host, urlpath) == 4) {
+	if(sscanf(argv[1], "ftp://%[^:]:%[^@]@%[^/]/%s", username, password, host, urlpath) == 4) {
 		printf("Full url detected.\n");
 	} else if(sscanf(argv[1], "ftp://%[^/]/%s", host urlpath) == 2) {
 		printf("Short url detected.\n");
+		username = "anonymous";
+		password = "anonymous";
 	} else {
 		printf("Something in your input was wrong =(\nTry to check your url, maybe?\n");
 		exit(1);
